8f5a577e9b646eb7259112bbe28b315d8e66825f
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 15:20:08 -0700

    Delete Unnecessary code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,5 +2,4 @@ export const getPokedexData = async () => {
   let url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
   const res = await fetch(url)
   return res
-    // .then(res => res.ok ? res.json() : this.displayErrorInfo(res))
 }
